776f8e7b7b6db9c415d7dd2e14e880b074683bf1
spencermathias <46462425+spencermathias@users.noreply.github.com>
2022-06-09 21:27:28 -0500

    add word entry box

--- boggleServer.js
+++ boggleServer.js
@@ -3,6 +3,7 @@ comms = require("allgameplugin")
 io = comms.createServer({keepSockets:true, standAlonePort:8080},(gameID)=>{return true})
 app = comms.clientFiles()
 app.use("./htmlBoggle")
+players={}
 
 standardGames={
     boggleDice_Classic :{rows:4,cols:4,subs:{'1':'Qu'},
@@ -23,23 +24,115 @@ standardGames={
 }
 
 options={
-    basegame:standardGames.boggleDice_Classic
+    basegame:standardGames.boggleDice_Classic,
+    time:3
 }
+var diceOrder=[];
+var compairWords=false
+var nodes=[]
+var checkedwords={}
+var grade=false
+
 io.sockets.on("connection", function(socket) {
+    if(players[socket.id]==undefined){
+        players[socket.id]={
+            mywords:new Set(),
+            name:socket.id,
+            symbol:String.fromCharCode(33+Object.keys(players).length)
+        }
+    }
+    socket.on("onReConnect",(data)=>{players[socket.id].name=(data!=undefined)?data:socket.id;console.log('myname',data)})
     socket.on('getLetters',()=>{
-        gameVals=options.basegame
-        total=gameVals.rows*gameVals.cols
-        diceOrder=[]
-        avalibleDice=Array.from(Array(total).keys())
+        players[socket.id].mywords=new Set()
+        diceOrder=[];
+        let gameVals=options.basegame
+        let total=gameVals.rows*gameVals.cols
+        let avalibleDice=Array.from(Array(total).keys())
         for(i = 0;i<total;i++){
-            nextDie = Math.floor(Math.random()*(total-i))
-            letter = gameVals.letters[nextDie][Math.floor(Math.random()*6)]
+            let nextDie = avalibleDice.splice(Math.floor(Math.random()*(total-i)),1)
+            let letter = gameVals.letters[nextDie][Math.floor(Math.random()*6)]
             if(gameVals.subs[letter]!=undefined){
                 letter = gameVals.subs[letter]
             }
             diceOrder.push(letter)
+            nodes.push({letter:letter,neighbors:[],position:i})
         } 
-        socket.emit('gameBoard',{rows:gameVals.rows,cols:gameVals.cols,letters:diceOrder})
+        for(let i=0;i<nodes.length;i++){
+            nodes[i].neighbors=[
+                nodes[i-1],nodes[i+1],
+                nodes[i-1-gameVals.cols],nodes[i-gameVals.cols],nodes[i-gameVals.cols+1],
+                nodes[i+gameVals.cols-1],nodes[i+gameVals.cols],nodes[i+gameVals.cols+1]
+            ]
+        }
+        io.sockets.emit('gameBoard',{rows:gameVals.rows,cols:gameVals.cols,letters:diceOrder})
+        countTime=options.time-1
+        io.sockets.emit("time",countTime)
+        var x = setInterval(function() {
+            countTime-=1
+            if (countTime < 0) {
+                clearInterval(x);
+                io.sockets.emit('gameBoard',{rows:0,cols:0,letters:[" "]})
+            }else{
+                io.sockets.emit("time",countTime)
+            }
+        }, 60000);
+        compairWords=false
+        io.sockets.emit('wordList',{dictionary:{},wordLists:[{words:Array.from(players[socket.id].mywords),symbol:"",name:"me"}]})
     });
+    socket.on("oldLetters",()=>{
+        let gameVals=options.basegame
+        io.sockets.emit('gameBoard',{rows:gameVals.rows,cols:gameVals.cols,letters:diceOrder})
+        compairWords=true
+        sendWords(socket)
+    });
+    socket.on("boggleWord",(text)=>{
+        let numwords=players[socket.id].mywords.size
+        for(word of text.split(" ")){
+            players[socket.id].mywords.add(word)
+            //checkedwords[word]=isValidWord(word)
+        }
+        if(players[socket.id].mywords.size>numwords){
+            sendWords(socket)
+        }
+        
+    })
+    socket.on(grade,()=>{grade^=grade})
+    socket.on("clearWords",()=>{players[socket.id]=new Set()})
     console.log(socket.id)
+
 })
+
+function sendWords(socket){
+    if(compairWords){
+        let emitList=Object.values(players).map((player)=>{
+            return {words:Array.from(player.mywords),symbol:player.symbol,name:player.name}
+        })
+        io.sockets.emit('wordList',{dictionary:grade ? checkedwords : {} ,wordLists:emitList})
+    }else{
+        socket.emit('wordList',{dictionary:{},wordLists:[{words:Array.from(players[socket.id].mywords),symbol:"",name:"me"}]})
+    }
+}
+
+function isValidWord(word){
+    let letters=word.toUpperCase().split("")
+    if(letters.reduce((previousVal,currentVal)=>{return previousVal&&(diceOrder.some(letter=>letter==currentVal.toUpperCase()))},true)){
+        let firstletters=nodes.filter(node=>node.letter==letters[0])
+        let positions=[]
+        firstletters.forEach(firstletter => {
+            let secondletters=[]
+            secondletters.push(...firstletter.neighbors.filter(node=>node.letter==letters[1]))
+            secondletters.forEach(secondletter=>{
+                let lastletters=[]
+                lastletters.push(...secondletter.neighbors.filter(node=>node.letter==letters[2]))
+                positions.push(lastletters.map(((lastletter)=>{
+                    return [firstletter.position,secondletter.position,lastletter.position]
+                })))                
+            })
+        });
+        return positions
+    }else{
+        return undefined
+    }
+}
+
+
--- htmlBoggle/boggle.js
+++ htmlBoggle/boggle.js
@@ -1,3 +1,4 @@
+let numCols=3
 socket=clientComms("localhost:80")
 socket.on("connect", function() {console.log(socket.id)})
 socket.on("gameBoard",function(data){
@@ -6,11 +7,97 @@ socket.on("gameBoard",function(data){
     for(i=0;i<data.rows;i++){
         let rowHTML="<tr>"
         for(j=0;j<data.cols;j++){
-            rowHTML+="<td>"+data.letters[k++]+"</td>"
+            rowHTML+="<td class='boggleBoard'>"+data.letters[k++]+"</td>"
         }
         $("#boggleLetters").append(rowHTML+"</tr>")
     }
     $("#boggleLetters").width(""+(data.cols*52)+"pt")
     console.log(data)
 })
-$("#newboard").click(function(){socket.emit("getLetters")})
+
+socket.on("time",function(minutes){
+    console.log("recived"+minutes)
+    countTime=59
+    let x = setInterval(function() {
+        $("#timer")[0].innerHTML = ""+minutes+":"+countTime
+        countTime-=1
+        if (countTime == 0) {
+          clearInterval(x);
+          $("#timer")[0].innerHTML = "expired"
+        }
+    }, 1000);
+})
+socket.on("wordList",(data)=>{
+    let lists = data.wordLists
+    if($("#wordLists tr th").length!=lists.length){
+        $("#wordLists tr").remove()
+        let headerRow="<tr>"
+        for(list of lists){
+            headerRow+="<th></th><th colspan="+lists.length+">"+list.name+"</th>"
+        }
+        headerRow+="</tr>"
+        $("#wordLists thead").append(headerRow)
+    }
+    if(lists.length == 1){
+        $("#wordLists tbody tr").remove()
+        let row="<tr>"
+        let i=1
+        for (let item of lists[0].words){
+            row+="<td>"+item+"</td>"
+            if(i==numCols){
+                row+="</tr>"
+                $("#wordLists tbody").append(row)
+                row="<tr>"
+            }
+            i%=numCols;i++
+        }
+        row+="</tr>"
+        $("#wordLists tbody").append(row)
+    }else{
+        $("#wordLists tbody tr").remove()
+        for(list of lists){
+            list.words=new Set(Array.from(list.words).sort((a, b) => a.localeCompare(b)))
+        }
+        let wordSymbolLists=lists.map((value,i,arr)=>{
+            return [...value.words].map((word)=>{
+                let wordSymbols=[]
+                for(let list_Ind in arr){
+                    if(list_Ind!=i){
+                        if(arr[list_Ind].words.has(word)){
+                            wordSymbols.push(arr[list_Ind].symbol)
+                        }else{
+                            wordSymbols.push(" ")
+                        }
+                    }
+                }
+                wordSymbols.push(word)
+                return wordSymbols
+            })
+        })
+        let length=Math.max(...wordSymbolLists.map((wordList)=>{return wordList.length}))
+        for(i=0;i<length;i++){
+            let row="<tr>"
+            for(j=0;j<wordSymbolLists.length;j++){
+                row+='<td width="30pt"></td><td>'
+                row+=((wordSymbolLists[j][i]!=undefined)?wordSymbolLists[j][i]:Array(wordSymbolLists.length).fill(" ")).join("</td><td>")
+                row+='</td>'
+            }
+            row+='</tr>'
+            $("#wordLists tbody").append(row)
+        }
+    }
+})
+socket.on("grade",(data)=>{
+    data.validWords
+    data.points
+
+})
+$("#newBoard").click(function(){socket.emit("getLetters")})
+$("#oldBoard").click(function(){socket.emit("oldLetters")})
+$('#submit').click(function(){socket.emit('message',$('#submit').val())})
+sendMessage=function(){
+	message=$('#message').val()         
+	socket.emit("boggleWord",message); 
+	$('#message').val('');
+	return false;
+};
